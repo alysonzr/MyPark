5c4728488b5696f97a862d74e69be5b70aadece5
Alyson <alyson.zancanaro@hotmail.com>
2019-09-06 23:29:10 -0300

    layout

--- app/src/main/java/com/example/mypark/LoginActivity.java
+++ app/src/main/java/com/example/mypark/LoginActivity.java
@@ -1,19 +1,21 @@
 package com.example.mypark;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 
+//PAGINA INICIAL APOS USUARIO LOGAR
 public class LoginActivity extends AppCompatActivity {
-    //PAGINA INICIAL APOS USUARIO LOGAR
-    TextView txtNomeUsuario;
-    EditText btnPesquisar = (EditText)findViewById(R.id.btnPesquisar);
 
+
+    Button btnPesquisar;
+    CheckBox checkAcademia,checkDog,checkSkate;
 
 
     @Override
@@ -21,27 +23,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        btnPesquisar = (Button)findViewById(R.id.btnPesquisar);
+        checkAcademia = (CheckBox)findViewById(R.id.checkAcademia);
+        checkDog = (CheckBox)findViewById(R.id.checkDog);
+        checkSkate = (CheckBox)findViewById(R.id.checkSkate);
+
         //Recebendo nome do usuario da main
         Intent i = getIntent();
         Bundle p = i.getExtras();
         String nome = p.getString("chave_nome");
-        setTitle("Bem Vindo, "+nome);
-
+        setTitle("Bem Vindo, " + nome);
 
         btnPesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(checkAcademia.isChecked()){
+                    Log.d("Teste", "onClick: Chek selecionado");
+                    Toast.makeText(LoginActivity.this, "Check academia selecionado", Toast.LENGTH_SHORT).show();
+                }
+                if(checkDog.isChecked()){
+                    Log.d("Teste", "onClick: Chek selecionado");
+                    Toast.makeText(LoginActivity.this, "Check brinquedos selecionado", Toast.LENGTH_SHORT).show();
+                }
 
+                if(!checkDog.isChecked()){
+                    Log.d("Teste", "onClick: Chek selecionado");
+                    Toast.makeText(LoginActivity.this, "Nenhum check selecionado", Toast.LENGTH_SHORT).show();
+                }
 
             }
 
+
         });
 
-
-
+    }
 
     }
 
 
 
-}
+
